d6e4aad7ff67d71fdd264c1eb778223bac8d6d0a
Azman Abdullah <azmanabdlh@yahoo.com>
2021-08-09 03:52:04 +0900

    (mengubah) boot.sh

--- boot.sh
+++ boot.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 cp .env.example .env
 composer install
+composer dump-autoload
 yarn install
 php artisan migrate:fresh
 php artisan db:seed
 php artisan key:generate
+php artisan storage:link
 
 echo 'Done ------>'
 
