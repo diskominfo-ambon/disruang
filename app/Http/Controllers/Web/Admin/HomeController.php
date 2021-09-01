166125707ceb4c8b34fd34a42daafc62d9d237d8
Azman Abdullah <azmanabdlh@yahoo.com>
2021-09-01 19:32:03 +0900

    (menambahkan) akses ke halaman beranda admin

--- app/Http/Controllers/Web/Admin/HomeController.php
+++ app/Http/Controllers/Web/Admin/HomeController.php
@@ -4,125 +4,40 @@ namespace App\Http\Controllers\Web\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Gate;
 
-use App\Models\Room;
+use App\Models\Schedule;
+use App\Models\User;
 
-class RoomsController extends Controller
+class HomeController extends Controller
 {
+  public function index()
+  {
+    $superUsersCount = User::role('admin')->count();
+    $usersCount = User::role('user')->count();
+    $schedules = Schedule::confirm();
 
-    public function __construct()
-    {
-      abort_if(Gate::allows(['humas', 'protocol']), 401);
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-      $keyword = $request->get('keyword', '');
-      $rooms = Room::where('name', 'like', "%{$keyword}%")
-        ->paginate(20);
-
-      $rooms->append('keyword');
-
-      return view('web::admin.rooms.index', compact('rooms', 'keyword'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-      return view('web::admin.rooms.new');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      $body = $request->validate([
-        'name' => 'required'
-      ], [], [
-        'name' => 'Nama ruangan'
-      ]);
-
-      $room = Room::create(
-        $body
-      );
-
-      return Redirect::route('admin.rooms.index')
-        ->with(
-          'message',
-          "Berhasil menambahkan ruangan {$room->name}"
-        );
-    }
+    // schedule in days.
+    $scheduleInDays = $schedules->limit(20)
+      ->get()
+      ->filter(fn ($schedule) => $schedule->started_at->isToday());
 
 
+    // schedule in weeks.
+    $scheduleInWeeks = $schedules->limit(20)
+      ->get()
+      ->filter(function ($schedule) {
+        $current = now();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Room $room)
-    {
-      return view('web::admin.rooms.edit', compact('room'));
-    }
+        return $schedule->started_at >= $current->startOfWeek()
+          && $schedule->started_at <= $current->endOfWeek();
+    });
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Room $room)
-    {
-      $body = $request->validate([
-        'name' => 'required'
-      ], [
-        'name' => 'Nama ruangan'
-      ]);
+    $schedule = (object) [
+      'weeks' => $scheduleInWeeks,
+      'days' => $scheduleInDays,
+      'all' => $schedules
+    ];
 
-      $room->update(
-        $body
-      );
-
-      return Redirect::route('admin.rooms.index')
-        ->with(
-          'message',
-          "Berhasil menyimpan perubahan ruangan {$room->name}"
-        );
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Room $room)
-    {
-      $message = "Berhasil menghapus ruangan {$room->name}";
-      // delete room in db.
-      $room->delete();
-
-      return Redirect::back()
-        ->with(
-          'message',
-          $message
-        );
-    }
+    return view('web::admin.home', compact('superUsersCount', 'usersCount', 'schedule'));
+  }
 }
