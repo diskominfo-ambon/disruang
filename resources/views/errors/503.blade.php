3b5e8941ec6ef191a057c9bde847397ae63f0101
Azman Abdullah <azmanabdlh@yahoo.com>
2021-09-24 14:17:34 +0900

    (menambahkan) halaman maintenance

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>😿 Tungguinn yaa</h1>
+    <div>
+        <p>Maaf atas ketidaknyamanannya, tetapi saat ini kami sedang melakukan beberapa pemeliharaan. Jika perlu, Anda selalu dapat menghubungi kami, jika tidak, kami akan segera kembali online!</p>
+        <p>&mdash; Team disruang</p>
+    </div>
+</article>
