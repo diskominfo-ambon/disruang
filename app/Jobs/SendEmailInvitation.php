2de60c78149d194d65847cb69fb1c58decb495c4
Azman Abdullah <azmanabdlh@yahoo.com>
2022-05-10 00:39:59 +0900

    (fix) namespace Mail pada Job:SendEmailInvitation

--- app/Jobs/SendEmailInvitation.php
+++ app/Jobs/SendEmailInvitation.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Mail\ScheduleParticipantInvitation;
+use Illuminate\Support\Facades\Mail;
 
 class SendEmailInvitation implements ShouldQueue
 {
